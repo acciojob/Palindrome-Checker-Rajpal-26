9bb52065c7ec91a0c96aaaae44026fdd62e9a816
Rajpal-26 <120132958+Rajpal-26@users.noreply.github.com>
2023-01-16 00:54:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // complete the given function
 
 function palindrome(str){
-	if(str.length<=0){
+	if(str.length== 0){
 		return false;
 	}
+	
 	let left =0;
 	let right = str.length-1;
 	while(left<right){
