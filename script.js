b71e76dfd3cefe25537139df370ea18ecb75665b
Rajpal-26 <120132958+Rajpal-26@users.noreply.github.com>
2023-01-16 00:20:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
-
+	let j = str.length-1;
+	let count=0;
+	for(let i=0;i<str.length;i++){
+		if(str.charAt[i]==str.charAt[j]){
+			count++;
+		}
+		else{
+			return false;
+		}
+		return true;
+	}
 }
 module.exports = palindrome
