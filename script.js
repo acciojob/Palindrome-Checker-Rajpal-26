a07bc874880eee35b5d2260e0f8f0e3125dcb3a9
Rajpal-26 <120132958+Rajpal-26@users.noreply.github.com>
2023-02-02 19:31:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function palindrome(str){
 	}
 	
 	let left =0;
-	let right = str.length;
-	while(left<right){
+	let right = str.length-1;
+	while(left<=right){
 		if(str[left]!==str[right]){
 			return false;
 		}
